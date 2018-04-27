da79fa6033d00e04be1307576ef121ef36f50922
Ignacio Delmont <ignaciodelmont@gmail.com>
2018-04-27 11:10:46 -0300

    added GET /:id route

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const {lawProject} = require('./models/lawProject');
 var app = express();
 const port = process.env.PORT || 3000;
 
+
+// GET every single law project
 app.get('/', (req, res) => {
   lawProject.find().then((projects) => {
     res.send(projects);
@@ -18,6 +20,24 @@ app.get('/', (req, res) => {
   });
 });
 
+
+// GET by Id a single project
+app.get('/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  lawProject.findById(id).then((project) => {
+    if (!project) {
+      return res.status(404).send();
+    }
+    res.send(project);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+// updates db from csv file
 fs.createReadStream('expedientes.csv')
   .pipe(csv())
   .on('data', (data) => {
