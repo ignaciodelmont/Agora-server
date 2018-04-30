1f24fbe8b96f883e2a1753e8c1e64eff32916c82
Ignacio Delmont <ignaciodelmont@gmail.com>
2018-04-30 12:18:46 -0300

    added comments and PATCH /:id/:vote route

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 3000;
 
 
 // GET every single law project
+// Ex: domain/
 app.get('/', (req, res) => {
   lawProject.find().then((projects) => {
     res.send(projects);
@@ -22,6 +23,7 @@ app.get('/', (req, res) => {
 
 
 // GET by Id a single project
+// Ex: domain/5adfe95d3a49cb56a269b195
 app.get('/:id', (req, res) => {
   var id = req.params.id;
   if (!ObjectID.isValid(id)) {
@@ -38,6 +40,7 @@ app.get('/:id', (req, res) => {
 });
 
 // DELETE by Id a single project
+// Ex: domain/5adfe95d3a49cb56a269b195
 app.delete('/:id', (req, res) => {
   var id = req.params.id;
   if(!ObjectID.isValid(id))
@@ -50,6 +53,29 @@ app.delete('/:id', (req, res) => {
     res.status(200).send();
     console.log(todo);
   },(e) => res.status(400).send());
+});
+
+// PATCH by Id a single project
+// Ex: domain/5adfe95d3a49cb56a269b195/favor or domain/5adfe95d3a49cb56a269b195/against
+app.patch('/:id/:vote', (req, res) => {
+  var data = {
+    "id": req.params.id,
+    "vote": req.params.vote
+  };
+  if (!ObjectID.isValid(data.id) || data.vote != 'favor' && data.vote != 'against')
+    return res.status(404).send();
+
+  lawProject.findByIdAndUpdate(data.id, {$inc: {[data.vote]: 1}}, {new: true}).then((project) => {
+    // to send a language variable as field it's necessary to place it between []
+    // (in this case data.vote, switches between favor and against values)
+    if (!project)
+      return res.status(404).send();
+
+    res.status(200).send({project});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
 });
 // updates db from csv file
 // fs.createReadStream('expedientes.csv')
