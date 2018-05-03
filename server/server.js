ce8c0183dbc7f3b10a40fd07ca461b0e240e1894
Ignacio Delmont <ignaciodelmont@gmail.com>
2018-05-02 23:30:28 -0300

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -104,6 +104,7 @@ app.patch('/projects/:id/:vote', (req, res) => {
 // /users ----------------------------------------------------------------------
 
 // POST /users
+// Ex: domain/users (body = {email, password, firstName, middleName, lastName, idNumber})
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email','password','firstName','middleName','lastName','idNumber']);
   console.log(req.body);
@@ -118,6 +119,7 @@ app.post('/users', (req, res) => {
   });
 });
 
+// Ex: domain/users (body = {email, password})
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
@@ -133,7 +135,16 @@ app.post('/users/login', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
-// Listening
-  app.listen(port, () => {
-    console.log(`Started on port ${port}`);
-  });
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.staus(400).send();
+  })
+});
+
+// Listening -------------------------------------------------------------------
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
