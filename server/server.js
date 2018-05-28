a5149de09ac31e5020c5c93c1301fd3d2c49d38d
Ignacio Delmont <ignaciodelmont@gmail.com>
2018-05-28 15:49:54 -0300

    Add Allow-Methods post,get,options,patch,delete

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Expose-Headers", "x-auth");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, DELETE"); 
   next();
 });
 
