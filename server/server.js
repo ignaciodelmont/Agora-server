9afce6eebb617d5f19369dbf7ba095fe83086650
Ignacio Delmont <ignaciodelmont@gmail.com>
2018-05-28 22:31:51 -0300

    Add allow-headers: x-auth

--- server/server.js
+++ server/server.js
@@ -18,9 +18,9 @@ var app = express();
 app.use([bodyParser.json(),logger]);
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth");
   res.header("Access-Control-Expose-Headers", "x-auth");
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, DELETE"); 
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, DELETE");
   next();
 });
 
